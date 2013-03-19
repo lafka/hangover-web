468146d1c6eaabb6f681a3115a08c227c88229c1
Olav Frengstad <olav@fwt.no>
2013-03-19 23:30:02 +0530

    tmp stuff, will split up later?

--- js/plugin/playlist/tpl/overview.tpl
+++ js/plugin/playlist/tpl/overview.tpl
@@ -6,10 +6,23 @@
 			Did you consider creating one (yeah i know, nowhere to go)....
 		</p>
 	</div>
-<% } else { %>
+<% } %>
+
+<%= createTemplate %>
+
+<% if (0 < data.length) { %>
 	<% _.each(data, function(elem) { %>
 		<div id="span12">
-			<h2><a href="#/playlist/<%= elem.id %>"><%= elem.name %></a></h2>
+			<h2>
+				<a href="#/playlist/<%= elem.id %>"><%= elem.name %></a>
+				<div class="btn-toolbar span2 pull-right">
+					<div class="btn-group">
+					<a class="<%= elem.tracks.length > 0 ? '' : 'disabled ' %>btn" href="#" title="Play"><i class="icon-headphones"></i></a>
+					<a class="btn" href="#" title="Edit playlist"><i class="icon-pencil"></i></a>
+					<a class="btn" href="#" title="Delete playlist"><i class="icon-trash"></i></a>
+					</div>
+				</div>
+			</h2>
 			<%	if ( elem.tracks.length < 10 ) { %>
 				<%= _.template(tpl, {data: elem.tracks}) %>
 			<% } else { %>
--- js/plugin/playlist/view/overview.js
+++ js/plugin/playlist/view/overview.js
@@ -4,24 +4,42 @@ define(
 	],
 	function(Backbone, App) {
 		var view = Backbone.View.extend({
+			model: undefined,
 			playlists: undefined,
 			initialize: function() {
 				this.playlists = new App.Collection.playlists();
+				this.model = new App.Model.playlist();
 			},
 			render: function() {
 				var view = this;
 
 				require([
 					"text!plugin/playlist/tpl/overview.tpl",
-					"text!plugin/tracks/tpl/search-table.tpl"
-				], function(canvas, table) {
+					"text!plugin/tracks/tpl/search-table.tpl",
+					"text!tpl/form.tpl"
+				], function(canvas, table, form) {
 					view.playlists.fetch({success: function(ev) {
-						view.renderCanvas.call(view, canvas, table);
+						view.renderCanvas.call(view, canvas, table, form);
 					}});
 				});
 			},
-			renderCanvas: function(canvas, table) {
+			renderCanvas: function(canvas, table, form) {
 				this.$el.html( _.template( canvas, {
+					createTemplate: _.template(form, {
+						title: "Create playlist",
+						elems: [
+							{
+								field: "name",
+								title: "Name",
+								placeholder: "Playlist name",
+								value: this.model.get("name")
+							}
+						],
+						alert: undefined,
+						alertTitle: "",
+						message: undefined,
+						button: "Create playlist"
+					}),
 					tpl: table,
 					data: this.playlists.toJSON()
 				}));
